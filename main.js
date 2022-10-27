dc278157797863861cb70a1ebb5ab7542ccc5781
ali <alielnagar822@gmail.com>
2022-10-27 18:31:42 +0200

    modified main.js

--- main.js
+++ main.js
@@ -372,3 +372,45 @@ console.log(a.endsWith("b",7 ));
 console.log(a.endsWith("eb",7));
 
 */
+
+/*
+let userName = "Elzero";
+
+console.log(userName[0].toLowerCase());
+console.log(userName[0].toLowerCase().repeat(3));
+console.log(userName.substring(0,1).toLowerCase());
+console.log(userName.substr(0,1).toLowerCase());
+console.log(userName.charAt(0).toLowerCase());
+console.log(userName.slice(0,1).toLowerCase());
+console.log(userName.slice(3,4));
+
+*/
+
+/*
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(word.includes(letterZ));
+console.log(word.startsWith(letterE.toUpperCase()));
+console.log(word.endsWith(letterO.toLowerCase()));
+
+*/
+
+let a = "Elzero Web School";
+
+console.log( a.charAt(2).toUpperCase() + a.slice(3,6));
+
+
+console.log(a.substr(-4,1).repeat(8).toUpperCase());
+
+
+console.log(a.split(" ",1));
+
+
+console.log(`${a.substr(0,6)} ${a.substr(-6,6)}`);
+
+
+console.log();
+
